fa706e27dbcea74b9ddff2e97d8d52b8d3718a6c
Senthuran100 <senthuran.manoharan@gmail.com>
2023-04-02 18:31:37 +1000

    modify the app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +9,4 @@ def hello_geek():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
